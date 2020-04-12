3a0a9f545e587eb8f1b954a0c36233fef602e8d5
EdmilsonSantana <edmilsonmgsantana@gmail.com>
2020-04-12 17:22:19 -0300

    Correçao de bug em caso de curriculo vazio.

--- resources/views/site/course/components/course-sidebar.blade.php
+++ resources/views/site/course/components/course-sidebar.blade.php
@@ -14,18 +14,23 @@
         <div class="course-feature-content">
             <div class="container">
                 <div class="d-flex flex-column">
+                    
+                    @if(!$is_subscribed)
                     <div class="course-pricing">
                         {{ $course->price ? 'R$'.$course->price : 'Gratuito' }}
                     </div>
+                    @endif
 
                     @php
 
-                    $first_lecture = array_values($curriculum_sections)[0][0];
-                    $route_course_content = url('course-enroll/'.$course->course_slug.'/'.SiteHelpers::encrypt_decrypt($first_lecture->lecture_quiz_id,true));
+                    $lecture_url = count($curriculum_sections) > 0 ? '/'.SiteHelpers::encrypt_decrypt(array_values($curriculum_sections)[0][0]->lecture_quiz_id,true) : '';
+
+                    $route_course_content = url('course-enroll/'.$course->course_slug.$lecture_url);
                     $route_course_checkout = route('course.checkout', $course->course_slug);
 
                     $btn_route = $is_subscribed ? $route_course_content : $route_course_checkout;
                     $btn_label = $is_subscribed ? 'Ir para o curso' : 'Inscreva-se agora';
+
                     @endphp
 
                     @include('admin/components/link',
@@ -41,7 +46,7 @@
     </section>
 
     <div class="lity-hide">
-        @include('site/course/components/course-video', ['video' => $video, 
-                'id' => 'course-video-preview', 'is_modal' => true])
+        @include('site/course/components/course-video', ['video' => $video,
+        'id' => 'course-video-preview', 'is_modal' => true])
     </div>
 </div>
--- resources/views/site/course/components/course-video.blade.php
+++ resources/views/site/course/components/course-video.blade.php
@@ -4,10 +4,8 @@
     @php
     $file_name = '/app/public/course/'.$video->course_id.'/'.$video->video_title.'.'.$video->video_type;
     @endphp
-    <div>{!! $file_name !!}</div>
     <video controls id="{{$id}}" preload="auto" class="video-js vjs-default-skin vjs-big-play-centered {{!$is_modal ? 'vjs-16-9' : ''}} " data-setup='{}'>
         <source src="{{ Storage::url($file_name) }}" type="video/mp4">
-        Your browser doesn't support HTML5 video tag.
         <p class="vjs-no-js">
             Para ver este vídeo, ative o JavaScript e considere atualizar para um navegador Web que
             <a href="https://videojs.com/html5-video-support/" target="_blank">suporte vídeo HTML5</a>
